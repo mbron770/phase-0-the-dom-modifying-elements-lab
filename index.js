2e15ab954d7b73a75e0c597e0f8c70d05d63c3bd
mbron770 <mbronfin.tsd@gmail.com>
2023-05-23 13:48:39 -0400

    add elements via dom

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+
+//remove the main div
+document.getElementById('main').remove()
+
+// add new header element
+
+const newHeader = document.createElement('h1') 
+newHeader.id = 'victory'
+newHeader.textContent = `Mordechai is the champion`
+document.body.appendChild(newHeader) 
+
+
+
+
+
+
+
+
+
